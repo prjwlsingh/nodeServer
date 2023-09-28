75fd8bd4b6fb5314affc741c5815f1c9c39e0139
prajwal <[prjwlsingh@gmail.com]>
2023-09-29 03:52:43 +0530

    modified api

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use((req, res, next) => {
 });
 
 // Route to fetch the first 15 GitHub users
-app.get('/users', async (req, res) => {
+app.get('/search', async (req, res) => {
+  const searchTerm = req.query.term; // Get the search term from the query parameter
   try {
     const response = await axios.get(`${githubApiUrl}?per_page=15`, {
       headers: {
@@ -30,14 +31,20 @@ app.get('/users', async (req, res) => {
     });
 
     const users = response.data;
-    res.json(users);
+    
+    // Filter users whose names have the search term as a prefix or suffix
+    const filteredUsers = users.filter(user => {
+      const lowerCaseName = user.login.toLowerCase();
+      const lowerCaseTerm = searchTerm.toLowerCase();
+      return lowerCaseName.startsWith(lowerCaseTerm) || lowerCaseName.endsWith(lowerCaseTerm);
+    });
+
+    // Sort filtered users by followers (you can choose 'followers' or any other criteria)
+    const sortedUsers = filteredUsers.sort((a, b) => b.followers - a.followers).slice(0, 15);
+
+    res.json(sortedUsers);
   } catch (error) {
     console.error('Error fetching GitHub users:', error);
     res.status(500).json({ error: 'Unable to fetch GitHub users' });
   }
-});
-
-// Start the Express.js server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
